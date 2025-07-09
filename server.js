cba64170a8290b68a222013e1c8c3f2a7b8e3177
cannizzoa <cannizzoa@gmail.com>
2025-07-09 12:12:11 +0200

    Added API Server

--- server.js
+++ server.js
@@ -4,12 +4,41 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 const app = express();
 
+// for API
+
+const { auth } = require("express-oauth2-jwt-bearer");
+const authConfig = require("./auth_config.json");
+
+// create the JWT middleware
+const checkJwt = auth({
+  audience: authConfig.audience,
+  issuerBaseURL: `https://${authConfig.domain}`
+});
+
+//
+
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(express.static(join(__dirname, "public")));
 
 app.get("/auth_config.json", (req, res) => {
   res.sendFile(join(__dirname, "auth_config.json"));
+});
+
+// API Route
+
+app.get("/api/external", checkJwt, (req, res) => {
+  res.send({
+    msg: "Your access token was successfully validated!"
+  });
+});
+
+app.use(function(err, req, res, next) {
+  if (err.name === "UnauthorizedError") {
+    return res.status(401).send({ msg: "Invalid token" });
+  }
+
+  next(err, req, res);
 });
 
 app.get("/*", (_, res) => {
@@ -19,5 +48,23 @@ app.get("/*", (_, res) => {
 process.on("SIGINT", function() {
   process.exit();
 });
+
+// API Route
+
+app.get("/api/external", checkJwt, (req, res) => {
+  res.send({
+    msg: "Your access token was successfully validated!"
+  });
+});
+
+app.use(function(err, req, res, next) {
+  if (err.name === "UnauthorizedError") {
+    return res.status(401).send({ msg: "Invalid token" });
+  }
+
+  next(err, req, res);
+});
+
+
 
 module.exports = app;
